132aa162fdf548ad4d550d37cf5387a15c7f4a9e
Ph Gomes <pedrinho.hg.silva2022@gmail.com>
2026-04-12 23:04:21 -0300

    Delete Atividade 02 - Metodos Em Java directory

--- Atividade 02 - Metodos Em Java/Atividade 02 - Metodos em Java/src/MetodoStatic.java
+++
@@ -1,9 +0,0 @@
-public class MetodoStatic {
-    public static void mensagemEstatica() {
-        System.out.println("Metodo estatico já esta por aqui!");
-    }
-
-    static void main(String[] args) {
-        MetodoStatic.mensagemEstatica();
-    }
-}
