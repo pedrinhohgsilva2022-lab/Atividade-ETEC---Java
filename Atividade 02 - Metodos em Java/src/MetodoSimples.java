0273f7ca0edf3cbc78a6eb8a94a4de455da87670
Ph Gomes <pedrinho.hg.silva2022@gmail.com>
2026-04-26 23:10:06 -0300

    Delete Atividade 02 - Metodos em Java directory

--- Atividade 02 - Metodos em Java/src/MetodoSimples.java
+++
@@ -1,9 +0,0 @@
-public class MetodoSimples {
-    public void imprimirNome(String nome) {
-        System.out.println("Nome recebido: " + nome);
-    }
-    public static void main(String[] args) {
-        MetodoSimples imp = new MetodoSimples();
-        imp.imprimirNome("Pedro");
-    }
-}
